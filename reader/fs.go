0db9f4323d531cc16cea14019067abc5dacfa483
thisisaaronland <straup+github@gmail.com>
2018-04-13 09:36:33 -0700

    initial commit

---
+++ reader/fs.go
@@ -0,0 +1,46 @@
+package reader
+
+import (
+	"errors"
+	wof_reader "github.com/whosonfirst/go-whosonfirst-readwrite/reader"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type FSReader struct {
+	wof_reader.Reader
+	root string
+}
+
+func NewFSReader(root string) (wof_reader.Reader, error) {
+
+	info, err := os.Stat(root)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !info.IsDir() {
+		return nil, errors.New("root is not a directory")
+	}
+
+	s := FSReader{
+		root: root,
+	}
+
+	return &s, nil
+}
+
+func (s *FSReader) Read(uri string) (io.ReadCloser, error) {
+
+	path := filepath.Join(s.root, uri)
+
+	_, err := os.Stat(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return os.Open(path)
+}
